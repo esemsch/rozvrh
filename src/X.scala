e92dde9b9c4fa6b303954e83a1fd34d55fdb2bcf
Eduard Semsch <fake@email.com>
2013-06-03 13:39:25 +0200

    more constraints

--- src/X.scala
+++ src/X.scala
@@ -31,60 +31,83 @@ object X extends App {
 
   def noSense = throw new RuntimeException("Does not make sense")
 
-  abstract class HourConstraint {
-    def valid(cls:Int,day:Int,hour:Int):Boolean
-    def preferred(cls:Int,day:Int,hour:Int):Boolean
-    def h(cls:Int,day:Int,hour:Int):Int
+  abstract class Constraint {
+    def valid:Boolean
+    def preferred:Boolean
+    def h:Int
   }
 
-  abstract class CrossHourConstraint {
-    def valid(day:Int,hour:Int):Boolean
-    def preferred(day:Int,hour:Int):Boolean
-    def h(day:Int,hour:Int):Int
+  abstract class NecessaryConstraint extends Constraint {
+    def preferred:Boolean = {valid}
+    def h:Int = {if(valid) 0 else 1000}
   }
 
-  abstract class DayConstraint {
-    def valid(cls:Int,day:Int):Boolean
-    def preferred(cls:Int,day:Int):Boolean
-    def h(cls:Int,day:Int):Int
-  }
-
-  abstract class CrossDayConstraint {
-    def valid(day:Int):Boolean
-    def preferred(day:Int):Boolean
-    def h(day:Int):Int
-  }
-
-  abstract class WeekConstraint {
-    def valid(cls:Int):Boolean
-    def preferred(cls:Int):Boolean
-    def h(cls:Int):Int
-  }
-
-  abstract class CrossWeekConstraint {
-    def valid():Boolean
-    def preferred():Boolean
-    def h():Int
-  }
-
-  class OneTeacherOneHour(val schoolSchedule:SchoolSchedule) extends CrossHourConstraint {
-    def valid(day: Int, hour: Int):Boolean = {
-      val d = schoolSchedule.hourSchedules(day,hour) filter (x => x!=null) map (x => x.teacher)
-      d.distinct.size == d.size
-    }
-    def preferred(day: Int, hour: Int):Boolean = {valid(day,hour)}
-    def h(day: Int, hour: Int):Int = {if(valid(day,hour)) 0 else 1000}
-  }
-
-  class SixthAndSeventhInCertainDaysOnly(val schoolSchedule:SchoolSchedule, days:Set[Int]) extends CrossWeekConstraint {
-    def valid: Boolean = {
+  class OdpoledniVUrciteDny(val schoolSchedule:SchoolSchedule, days:Set[Int]) extends NecessaryConstraint {
+    def valid = {
       schoolSchedule.schoolSchedule forall (x => x.classSchedule.zipWithIndex forall (x => {
         val daySch = x._1
         !((daySch(6)!=null || daySch(7)!=null) && !days.contains(x._2))
       }))
     }
-    def preferred: Boolean = valid()
-    def h(): Int = if(valid()) 0 else 1000
+  }
+
+  class VolnaHodina(val schoolSchedule:SchoolSchedule) extends NecessaryConstraint {
+    def valid = {
+      schoolSchedule.schoolSchedule forall(x => x.classSchedule forall (y => (y(5)==null || y(6)==null)))
+    }
+  }
+
+  class NepreruseneVyucovaniDopoledne(val schoolSchedule:SchoolSchedule) extends NecessaryConstraint {
+    def valid = {
+      schoolSchedule.schoolSchedule forall (x => x.classSchedule forall (y => 1 to 4 forall (z => y(z)!=null)))
+    }
+  }
+
+  def morningSchoolNtimes(cs:ClassSchedule,ntimes:Int) = {
+    cs.classSchedule.filter(x => x(0)!=null).size<=ntimes
+  }
+
+  def afternoonSchoolNtimes(cs:ClassSchedule,ntimes:Int) = {
+    cs.classSchedule.filter(ds => (ds(6)!=null || ds(7)!=null)).size<=ntimes
+  }
+
+  class PrvniDruha(val schoolSchedule:SchoolSchedule) extends NecessaryConstraint {
+    def valid = {
+      0 to 1 forall (i => morningSchoolNtimes(schoolSchedule.schoolSchedule(i),0) && afternoonSchoolNtimes(schoolSchedule.schoolSchedule(i),0))
+    }
+  }
+
+  class Treti(val schoolSchedule:SchoolSchedule) extends NecessaryConstraint {
+    def valid = {
+      morningSchoolNtimes(schoolSchedule.schoolSchedule(2),1) && afternoonSchoolNtimes(schoolSchedule.schoolSchedule(2),0)
+    }
+  }
+
+  class CtvrtaPata(val schoolSchedule:SchoolSchedule) extends NecessaryConstraint {
+    def valid = {
+      3 to 4 forall (i => morningSchoolNtimes(schoolSchedule.schoolSchedule(i),1) && afternoonSchoolNtimes(schoolSchedule.schoolSchedule(i),1))
+    }
+  }
+
+  class JedenUcitelJednaHodina(val schoolSchedule:SchoolSchedule) extends NecessaryConstraint {
+    def valid = {
+      0 to 4 forall (i => 0 to 7 forall (j => {
+        val aux = schoolSchedule.schoolSchedule.map(cs => cs.classSchedule(i)(j)).filter(tj=>tj!=null).map(_.teacher)
+        aux.distinct.size == aux.size
+      }))
+    }
+  }
+
+  class UcitelUciVUrciteDny(val schoolSchedule:SchoolSchedule, t:Teacher, days:Set[Int]) extends NecessaryConstraint {
+    def valid = {
+      schoolSchedule.schoolSchedule forall (x => ((0 to 4).toSet -- days) forall (i => x.classSchedule(i) forall (tj => tj == null || tj.teacher!=t)))
+    }
+  }
+
+  class UcitelUciVUrciteHodiny(val schoolSchedule:SchoolSchedule, t:Teacher, hours:Set[Int]) extends NecessaryConstraint {
+    def valid = {
+
+    }
   }
 
   val schoolSchedule: SchoolSchedule = new SchoolSchedule()
@@ -100,14 +123,24 @@ object X extends App {
   val tj21 = new TeachersJob(t2,ch1)
   val tj22 = new TeachersJob(t2,ch2)
 
-  schoolSchedule.schoolSchedule(0).classSchedule(0)(0) = tj11
-  schoolSchedule.schoolSchedule(1).classSchedule(0)(0) = tj22
-  schoolSchedule.schoolSchedule(1).classSchedule(1)(5) = tj22
+  schoolSchedule.schoolSchedule(0).classSchedule(0)(1) = tj11
+  schoolSchedule.schoolSchedule(1).classSchedule(0)(1) = tj22
+  schoolSchedule.schoolSchedule(1).classSchedule(0)(5) = tj22
+  schoolSchedule.schoolSchedule(2).classSchedule(0)(6) = tj22
+  schoolSchedule.schoolSchedule(2).classSchedule(0)(4) = tj22
 
-  val c = new OneTeacherOneHour(schoolSchedule)
-  val odpol = new SixthAndSeventhInCertainDaysOnly(schoolSchedule,Set(0,3))
+  val odpol = new OdpoledniVUrciteDny(schoolSchedule,Set(0,3))
+  val volna = new VolnaHodina(schoolSchedule)
+  val prvniDruha = new PrvniDruha(schoolSchedule)
+  val neprerusene = new NepreruseneVyucovaniDopoledne(schoolSchedule)
+  val t2UciJenVPo = new UcitelUciVUrciteDny(schoolSchedule,t2,Set(0))
+  val exklusivitaUcitele = new JedenUcitelJednaHodina(schoolSchedule)
 
-  println(c.valid(0,0))
   println(odpol.valid)
+  println(volna.valid)
+  println(prvniDruha.valid)
+  println(neprerusene.valid)
+  println(t2UciJenVPo.valid)
+  println(exklusivitaUcitele.valid)
 
 }

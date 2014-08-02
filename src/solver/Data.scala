bb4e6d89539b8008ecd5102dd747b8c30ea69e85
eduard <eduard.semsch@gmail.com>
2014-08-02 11:56:34 +0100

    added teachers' assignments for 2014

--- src/solver/Data.scala
+++ src/solver/Data.scala
@@ -77,8 +77,17 @@ object Data {
     Job(Teacher(teacher),createClassHour(s),cnt)
   }
 
-  def data2 = {
+  def data2 = data2013
 
+  def data = {
+    val teachers = Set("Tereza","Alena","Hana","Gita","Martina","Bohunka","Lucka","Eva","Iva").map(n => Teacher(n))
+
+    var teachersJobs = data2.flatMap(_.toTeachersJobs)
+
+    (teachersJobs,teachers)
+  }
+
+  def data2013 = {
     val TerezaJobs = Map(
       "Aj 8/9" -> 1,
       "Aj 8" -> 2,
@@ -197,12 +206,116 @@ object Data {
     TerezaJobs ++ AlenaJobs ++ HanaJobs ++ GitaJobs ++ MartinaJobs ++ BohunkaJobs ++ LuckaJobs ++ EvaJobs ++ IvaJobs
   }
 
-  def data = {
-    val teachers = Set("Tereza","Alena","Hana","Gita","Martina","Bohunka","Lucka","Eva","Iva").map(n => Teacher(n))
+  def data2014 {
+    val TerezaJobs = Map(
+      "Aj 6/7" -> 2,
+      "Aj 6" -> 1,
+      "Aj 7" -> 1,
+      "Aj 8/9" -> 2,
+      "Aj 8" -> 1,
+      "Aj 9" -> 1,
+      "Čj 6/7" -> 2,
+      "Čj 6" -> 2,
+      "Čj 7" -> 2,
+      "Hv 8/9" -> 1,
+      "Hv 1/2" -> 1
+    ).map(e => createJob(e._1,e._2,"Tereza")).toList
 
-    var teachersJobs = data2.flatMap(_.toTeachersJobs)
+    val MatematikaJobs = Map(
+      "M 6/7" -> 3,
+      "M 6" -> 2,
+      "M 7" -> 2,
+      "M 8/9" -> 3,
+      "M 8" -> 2,
+      "M 9" -> 2,
+      "F 6/7" -> 2,
+      "F 8/9" -> 1,
+      "F 8" -> 1,
+      "F 9" -> 1,
+      "Inf 8/9" -> 1,
+      "Inf 5/6" -> 1,
+      "Vo 6/7" -> 1
+    ).map(e => createJob(e._1,e._2,"Matematika")).toList
 
-    (teachersJobs,teachers)
+    val HanaJobs = Map(
+      "D 6/7" -> 2,
+      "D 8/9" -> 1,
+      "D 8" -> 1,
+      "D 9" -> 1,
+      "Z 6/7" -> 2,
+      "Z 8/9" -> 2,
+      "Tv_Chl 6/7/8/9" -> 2,
+      "Tv_Dív 6/7/8/9" -> 2,
+      "Pč 3/4/5" -> 1,
+      "Pč 6/7" -> 1,
+      "Pč 8/9" -> 1,
+      "Vv 3/4/5" -> 1,
+      "Vv 4/5" -> 1,
+      "Vv 6/7" -> 2,
+      "Vo 8/9" -> 1
+    ).map(e => createJob(e._1,e._2,"Hana")).toList
+
+    val GitaJobs = Map(
+      "Čj 3/4/5" -> 7,
+      "Čj 3/4" -> 1,
+      "Čj 3" -> 1,
+      "M 3/4/5" -> 5,
+      "Vl 4/5" -> 2,
+      "Tv 3/4/5" -> 2,
+      "Hv 3/4/5" -> 1,
+      "TV 1/2" -> 2
+    ).map(e => createJob(e._1,e._2,"Gita")).toList
+
+    val MartinaJobs = Map(
+      "Čj 1/2" -> 9,
+      "Čj 2" -> 1,
+      "M 1/2" -> 4,
+      "M 2" -> 1,
+      "Vv 1/2" -> 1,
+      "Pč 1/2" -> 1,
+      "Aj 3/5" -> 3,
+      "Aj 4" -> 3
+    ).map(e => createJob(e._1,e._2,"Martina")).toList
+
+    val BohunkaJobs = Map(
+      "Čj 8/9" -> 2,
+      "Čj 8" -> 2,
+      "Čj 9" -> 2,
+      "RV 7/8/9" -> 1,
+      "VV 8/9" -> 1
+    ).map(e => createJob(e._1,e._2,"Bohunka")).toList
+
+    val LuckaJobs = Map(
+      "Př 6/7" -> 2,
+      "Př 8/9" -> 1,
+      "Př 8" -> 1,
+      "Př 9" -> 1,
+      "Ch 8/9" -> 1,
+      "Ch 8" -> 1,
+      "Ch 9" -> 1,
+      "Př 4/5" -> 1,
+      "Př/Prv 5/3" -> 1,
+      "Prv 1/2/3" -> 2,
+      "Hv 6/7" -> 1
+    ).map(e => createJob(e._1,e._2,"Lucka")).toList
+
+    val EvaJobs = Map(
+      "Inf 5/6" -> 1,
+      "Inf 9" -> 2,
+      "Inf 8" -> 1,
+      "Vo 8/9" -> 1,
+      "Rv 8/9" -> 1,
+      "Pč 4/5" -> 1,
+      "Pč 6/7" -> 1,
+      "Pč 8/9" -> 1
+    ).map(e => createJob(e._1,e._2,"Eva")).toList
+
+    val IvaJobs = Map(
+      "Rj 7/8" -> 2,
+      "Rj 9" -> 2
+    ).map(e => createJob(e._1,e._2,"Iva")).toList
+
+    TerezaJobs ++ MatematikaJobs ++ HanaJobs ++ GitaJobs ++ MartinaJobs ++ BohunkaJobs ++ LuckaJobs ++ EvaJobs ++ IvaJobs
   }
 
 }
